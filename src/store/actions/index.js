2a8a7601b8d471c892ab6dd8665572f8c6e834b3
BokiGaja <bgajic93@gmail.com>
2019-03-23 10:48:51 +0100

    Persistent Auth state with local storage

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,3 +1,3 @@
 export { addIngredient, removeIngredient, initIngridients } from './burgerBuilder'
 export { purchaseBurger, purchaseBurgerSuccess, purchaseBurgerFail, purchaseInit, fetchOrders } from './order'
-export { auth, logout, setAuthRedirectPath } from './auth'
+export { auth, logout, setAuthRedirectPath, authCheckState } from './auth'
