922fcaefa543127160c6dc7a42f6b19d9d969f23
BokiGaja <bgajic93@gmail.com>
2019-03-23 12:42:52 +0100

    Enzym installed and test written

---
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -0,0 +1,16 @@
+// Enzyme let's us render only one component, not whole App
+// Shallow renders content of component, but not deeply
+import React from 'react';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import NavigationItems from './NavigationItems';
+import NavigationItem from './NavigationItem/NavigationItem'
+
+configure({ adapter: new Adapter()});
+
+describe('<NavigationItems />', () => {
+    it('should render two <NavigationItem /> elements if not authenticated', () => {
+        const wrapper = shallow(<NavigationItems/>);
+        expect(wrapper.find(NavigationItem)).toHaveLength(2);
+    })
+});
